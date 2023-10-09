bad378b4807a9305155459eed3195ebd9ce50096
carlosxmerca <eduarmercado4@gmail.com>
2023-10-08 22:13:16 -0600

    Add: newton raphson method :sparkles:

--- 5_derivadas.pl
+++ 5_derivadas.pl
@@ -46,10 +46,42 @@ de(X, F, FP) :-
     d(X, F, FP),
     assert(funcion_derivada(X, F, FP)).
 
-derivar(F, X0, R) :-
-    de(X, F, Derivada),
-    X is X0,
-    R is Derivada.
 
 ver_funciones_derivadas() :-
     listing(funcion_derivada).
+
+% Implementación del método de Newton-Raphson
+newton_raphson_start(X, F, X0, Tolerancia, Raiz) :-
+    de(X, F, _),
+    % funcion_derivada(X, FS, FP),
+    newton_raphson(X0, Tolerancia, Raiz).
+
+% Caso base: detenerse cuando la diferencia entre X0 y X1 es menor que la tolerancia
+newton_raphson(X0, Tolerancia, X1) :-
+    funcion_derivada(X, F, FP),
+    X is X0,
+    Fx0 is F,
+    X1 is X0 - Fx0 / FP,
+    abs(X1 - X0) < Tolerancia, 
+    !.
+
+% Paso de iteración del método de Newton-Raphson
+newton_raphson(X0, Tolerancia, Raiz) :-
+    funcion_derivada(X, F, FP),
+    % writeln("Iterando"),
+    % write("F: "), write(F),
+    % writeln(""),
+    % write("FP: "), write(FP),
+    % writeln(""),
+    X is X0,
+    Fx0 is F,
+    X1 is X0 - Fx0 / FP,
+    % write("Fx0: "), write(Fx0),
+    % writeln(""),
+    % write("X0: "), write(X0),
+    % writeln(""),
+    % write("FP: "), write(FP),
+    % writeln(""),
+    % write("X1: "), write(X1),
+    % writeln(""),
+    newton_raphson(X1, Tolerancia, Raiz).
