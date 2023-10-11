237f6c29e13e4f44ff3f422e89a77fdf52de712a
carlosxmerca <eduarmercado4@gmail.com>
2023-10-10 23:26:01 -0600

    Add: ABB operations

--- 7_arboles.pl
+++ 7_arboles.pl
@@ -1,3 +1,36 @@
+% Recorrer un ABB in-order
+mostrarArbolInorden([]).
+mostrarArbolInorden([SI, Dato, SD]) :-
+    mostrarArbolInorden(SI),
+    write(Dato), write(' '),
+    mostrarArbolInorden(SD).
+
+% Altura de un ABB
+altura([], 0) :- !.
+altura([SI, _, SD], H) :-
+    altura(SI, HI),
+    altura(SD, HD),
+    is(H, +(1, max(HI, HD))).
+
+% Busqueda de un elemento en el ABB
+buscarABB(Elemento, []) :-
+    write('El elemento: '), write(Elemento), 
+    write(', NO se encuentra en el arbol'),
+    !.
+
+buscarABB(Elemento, [_,Elemento,_]) :-
+    write('Se ha encontrado el elemento: '), write(Elemento),
+    !.
+
+buscarABB(Elemento, [SI, Dato, _]) :-
+    <(Elemento, Dato),
+    buscarABB(Elemento, SI),
+    !.
+
+buscarABB(Elemento, [_, Dato, SD]) :-
+    >(Elemento, Dato),
+    buscarABB(Elemento, SD).
+
 % Insertar nodo en un arbol
 ins_abb([SI, Dato, SD], Dato, [SI, Dato, SD]) :-
     writeln("El dato ya se encuentran en en el arbol"),
